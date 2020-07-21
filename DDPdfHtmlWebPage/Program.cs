0b22b347238f0ea95696cdc4eaba5d7d093148c8
Kuni <kuni.ujiie@grapecity.com>
2020-07-21 13:34:46 +0900

    プロジェクト ファイルを追加します。

---
+++ DDPdfHtmlWebPage/Program.cs
@@ -0,0 +1,24 @@
+﻿using GrapeCity.Documents.Html;
+using GrapeCity.Documents.Pdf;
+using System;
+
+namespace DDPdfHtmlWebPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GcPdfDocument.SetLicenseKey("");
+
+            var pdffile = @"HTMLPageToPDF.pdf";
+            var jpegfile = @"HTMLPageToPDF.jpeg";
+            var pngfile = @"HTMLPageToPDF.png";
+            var uri = new Uri(@"https://docs.grapecity.com/help/diodocs/pdf/#overview.html");
+            var gcHtmlRenderer = new GcHtmlRenderer(uri);
+
+            gcHtmlRenderer.RenderToPdf(pdffile, new PdfSettings());
+            gcHtmlRenderer.RenderToJpeg(jpegfile, new JpegSettings());
+            gcHtmlRenderer.RenderToPng(pngfile, new PngSettings());
+        }
+    }
+}
